02f619f2ebc995ece0a255a6d2de8ccce78a6dfd
lclc98 <liam.c14785@gmail.com>
2019-09-19 22:28:07 +1000

    Added domain to user endpoint

--- src/main/java/com/plumealerts/api/endpoints/v1/user/UserAPI.java
+++ src/main/java/com/plumealerts/api/endpoints/v1/user/UserAPI.java
@@ -1,6 +1,10 @@
 package com.plumealerts.api.endpoints.v1.user;
 
+import com.plumealerts.api.db.tables.records.UsersRecord;
 import com.plumealerts.api.endpoints.v1.domain.Domain;
+import com.plumealerts.api.endpoints.v1.domain.error.ErrorType;
+import com.plumealerts.api.endpoints.v1.user.domain.User;
+import com.plumealerts.api.handler.HandlerUser;
 import com.plumealerts.api.handler.user.DataError;
 import com.plumealerts.api.utils.ResponseUtil;
 import com.plumealerts.api.utils.TokenValidator;
@@ -14,11 +18,15 @@ public class UserAPI extends RoutingHandler {
     }
 
     private Domain getUser(HttpServerExchange exchange) {
-        DataError dataError = TokenValidator.getUserIdFromAccessToken(exchange);
+        DataError<String> dataError = TokenValidator.getUserIdFromAccessToken(exchange);
         if (dataError.hasError()) {
             return dataError.getError();
         }
-
-        return ResponseUtil.successResponse(exchange, null);
+        String userId = dataError.getData();
+        UsersRecord user = HandlerUser.findUser(userId);
+        if (user == null) {
+            return ResponseUtil.errorResponse(exchange, ErrorType.INTERNAL_SERVER_ERROR, null);
+        }
+        return ResponseUtil.successResponse(exchange, new User(user.getId(), user.getLogin(), user.getDisplayName(), user.getBeta(), user.getBroadcasterType()));
     }
 }
