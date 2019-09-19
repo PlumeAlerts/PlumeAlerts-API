62f0baedcc9a7a1f1892258b324c3887f11bad67
lclc98 <liam.c14785@gmail.com>
2019-09-19 22:28:07 +1000

    Added domain to user endpoint

--- src/main/java/com/plumealerts/api/endpoints/v1/user/domain/User.java
+++ src/main/java/com/plumealerts/api/endpoints/v1/user/domain/User.java
@@ -3,4 +3,17 @@ package com.plumealerts.api.endpoints.v1.user.domain;
 import com.plumealerts.api.endpoints.v1.domain.Domain;
 
 public class User extends Domain {
+    private final String id;
+    private final String login;
+    private final String displayName;
+    private final Boolean beta;
+    private final String broadcasterType;
+
+    public User(String id, String login, String displayName, Boolean beta, String broadcasterType) {
+        this.id = id;
+        this.login = login;
+        this.displayName = displayName;
+        this.beta = beta;
+        this.broadcasterType = broadcasterType;
+    }
 }
